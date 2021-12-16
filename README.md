d833b9df4907e991fd97ea5865d75761de5c45d7
geoff1337 <geoffreyloh@gmail.com>
2021-12-16 14:27:21 +1300

    update with rds and readme

--- README.md
+++ README.md
@@ -2,22 +2,22 @@
 
 ## Overview
 
-My solution to the Tech Challenge app. I haven't yet finished getting the database set up properly
+My solution to the Tech Challenge app. It is invoked from deploy.sh and requires aws cli and docker installed
 
-### TODO
+Master cfn script which runs nested scripts to do the following:
 
-* Provision the database first and output parameter for endpoint
-* Use endpoint parameter for db host in conf.toml file
-* continue with deploy.sh script
+* It deploys a VPC, with 2 public and private subnets across 2 Availabilty Zones.
+* An Internet Gateway, with a default route on the public subnets.
+* 2 NAT Gateways (1 in each AZ) and default routes for them in the private subnets.
+* A highly available ECS cluster using an AutoScaling Group, with ECS hosts distributed across 2 Availability Zones.
+* A Multi-AZ RDS
 
-### What it does right now
-
-* Builds the docker image and pushes it to ecr
-* Create s3 bucket for cfn files which are stored there for referencing as nested stacks
-* Deploy cloudformation template which runs through all the files posted to s3
+* Builds a docker image from dockerfile and pushes it to ecr
+* Gets the database endpoint URL from the above RDS and sets it in conf.toml
+* Deploys a service for the ECS cluster which pulls the docker image from ecr
 
 ### How to use it
 
-1. Deploys to AWS
-2. In the deploy.sh script at the top where it says 'set env variables (to change)' set these for your region, aws_account_id and s3_bucket_name which you wish to create to your account
+1. Install aws cli and docker
+2. In the deploy.sh script at the top where it says 'set env variables (to change)' set these for your region, aws_account_id and s3_bucket_name (must be unique) which you wish to create to your account
 3. Run deploy.sh
--- deploy.sh
+++ deploy.sh
@@ -8,11 +8,30 @@ s3_bucket_name=servian-geoff1337
 # other variables
 DbUser=postgres
 DbPassword=changeme
-ContainerPort=3000
+DbName=mydb
+
+# create s3 bucket for cloudformation templates
+aws s3api create-bucket --bucket $s3_bucket_name --region ${region} --create-bucket-configuration LocationConstraint=${region}
+
+# copy cfn files to s3
+aws s3 cp cfn s3://${s3_bucket_name}/ --recursive
+
+# deploy cfn stack
+aws cloudformation deploy --template-file cfn/all.yaml --stack-name servian-stack --parameter-overrides S3BucketName=${s3_bucket_name} Region=${region} DbUser=${DbUser} DbPassword=${DbPassword} DBName=${DbName} --capabilities CAPABILITY_NAMED_IAM
+
+# get rds endpoint url and append to conf file
+endpoint_url=`aws cloudformation describe-stacks --query Stacks[].Outputs[?OutputKey==\'RDS\'].OutputValue --output text`
+echo "\"DbHost\" = \"${endpoint_url}\"" >> conf.toml
 
 # build docker image
 imageName=servian/techchallengeapp
 docker build . -t ${imageName}:latest
+
+# revert conf file back to without rds endpoint
+sed -i '$ d' conf.toml
+
+# add data to db
+docker run ${imageName} updatedb -s
 
 # create ecr repo for docker image
 aws ecr create-repository --repository-name $imageName
@@ -24,11 +43,12 @@ image=${aws_account_id}.dkr.ecr.${region}.amazonaws.com/${imageName}:latest
 docker tag ${imageName}:latest ${image}
 docker push ${image}
 
-# create s3 bucket for cloudformation templates
-aws s3api create-bucket --bucket $s3_bucket_name --region ${region} --create-bucket-configuration LocationConstraint=${region}
+# create service from pushed docker image
+ContainerPort=3000
+VPC=`aws cloudformation describe-stacks --query Stacks[].Outputs[?OutputKey==\'VPC\'].OutputValue --output text`
+Cluster=`aws cloudformation describe-stacks --query Stacks[].Outputs[?OutputKey==\'Cluster\'].OutputValue --output text`
+Listener=`aws cloudformation describe-stacks --query Stacks[].Outputs[?OutputKey==\'Listener\'].OutputValue --output text`
+ECSServiceAutoScalingRoleARN=`aws cloudformation describe-stacks --query Stacks[].Outputs[?OutputKey==\'ECSServiceAutoScalingRole\'].OutputValue --output text`
+DesiredCount=2
 
-# copy cfn files to s3
-aws s3 cp cfn s3://${s3_bucket_name}/ --recursive
-
-# deploy cfn stack
-aws cloudformation deploy --template-file cfn/all.yaml --stack-name servian-stack --parameter-overrides S3BucketName=${s3_bucket_name} Region=${region} DbUser=${DbUser} DbPassword=${DbPassword} Image=${image} ContainerPort=${ContainerPort} --capabilities CAPABILITY_NAMED_IAM
+aws cloudformation deploy --template-file cfn/service.yaml --stack-name service-stack --parameter-overrides VPC=${VPC} Cluster=${Cluster} DesiredCount=${DesiredCount} Listener=${Listener} ECSServiceAutoScalingRoleARN=${ECSServiceAutoScalingRoleARN} Image=${image} ContainerPort=${ContainerPort} --capabilities CAPABILITY_NAMED_IAM
